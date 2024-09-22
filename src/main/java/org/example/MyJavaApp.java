b029cef615f93e9f030110a968a7e8d8b9fc6c05
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 17:59:33 -0400

    Migrating to .NET 8

--- src/main/java/org/example/MyJavaApp.java
+++ src/main/java/org/example/MyJavaApp.java
@@ -1,14 +1,13 @@
+
 package org.example;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 public class MyJavaApp {
     public static void main(String[] args) {
@@ -21,9 +20,9 @@ public class MyJavaApp {
         // Convert my Rochester object to XML
         writeXML(rochester);
 
-        // Ecode/decode my forcast
-        String encodedForcast = encode(rochester.getForecast());
-        decode(encodedForcast);
+        // Encode/decode my forecast
+        String encodedForecast = encode(rochester.getForecast());
+        decode(encodedForecast);
     }
 
     public static void writeXML(Weather weather) {
@@ -37,12 +36,10 @@ public class MyJavaApp {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
-
     }
 
     public static String encode(String originalString) {
-        BASE64Encoder encoder = new BASE64Encoder();
-        String encodedString = encoder.encode(originalString.getBytes());
+        String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
         System.out.println("Original String: " + originalString);
         System.out.println("Encoded String:  " + encodedString);
         System.out.println("============================================");
@@ -52,9 +49,8 @@ public class MyJavaApp {
     public static String decode(String encodedString) {
         String decodedString = "";
         try {
-            byte[] decodedBytes = new BASE64Decoder().decodeBuffer(encodedString);
-            decodedString = new String(decodedBytes, Charset.forName("UTF-8"));
-
+            byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+            decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
