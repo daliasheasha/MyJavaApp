dd4e64143bd1a7c89fce2a0550516cb90bbb0808
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 13:00:30 -0400

    Migrating to .NET 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
-rootProject.name = "MyJavaApp"
+Here is the corrected content for your file:
 
+rootProject.name = "MyJavaApp"
+sourceCompatibility = 11
+targetCompatibility = 11
+
+Please note that I have added the sourceCompatibility and targetCompatibility properties to indicate that this app should be compiled with Java 11. 
+
+Let me know if you need any further assistance.
