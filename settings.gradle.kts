9c6bc8e3d4220ba70dfb9e1c3e26697ed48931b5
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 13:11:36 -0400

    Migrating to .NET 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
-rootProject.name = "MyJavaApp"
+The given file appears to be a Gradle build file. To migrate the app to Java 11, you need to update the Java version in the build file.
 
+Here is the updated content for the build file:
+
+```
+rootProject.name = "MyJavaApp"
+sourceCompatibility = 11
+targetCompatibility = 11
+```
+
+Please replace the existing content in your build file with the updated content above.
+
+If you have any other files or configurations that need to be modified for the migration, please provide them, and I will assist you further.
