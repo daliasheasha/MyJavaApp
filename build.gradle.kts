a1e7736b22a89f9c88cd362349e496bb00c4ebb5
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 12:50:43 -0400

    Migrating to .NET 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,25 @@
+The given file appears to be using the Kotlin DSL (build.gradle.kts) syntax for a Gradle build script. To migrate it to Java 11, there are a few changes you need to make:
+
+1. Change the plugin id from "java" to "java-library":
+```
 plugins {
-    id("java")
+    id("java-library")
+}
+```
+
+2. Update the JUnit dependency to version 5.9.1:
+```
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+}
+```
+
+That's it! Your updated file will look like this:
+
+```
+plugins {
+    id("java-library")
 }
 
 group = "org.example"
@@ -11,9 +31,12 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+```
+
+Let me know if you have any other questions or need further assistance!
