0571341a9e438daa2fc67575e67da5f95a567573
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-23 06:53:16 -0400

    Migrating to .NET 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
+
 rootProject.name = "MyJavaApp"
 
+sourceCompatibility = 11
+targetCompatibility = 11
+
+repositories {
+    // your repositories configuration
+}
+
+dependencies {
+    // your dependencies configuration
+}
+
+// any other build configurations needed
