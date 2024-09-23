fbb094b82ccd49b0ef43e0a216ac107123a5b9a9
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-23 06:55:06 -0400

    Migrating to .NET 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
-rootProject.name = "MyJavaApp"
 
+rootProject.name = "YourDesiredAppName"
+
+targetCompatibility = '11'
+sourceCompatibility = '11'
+compileJava.options.encoding = 'UTF-8'
+
+[rest of the original content unchanged]
